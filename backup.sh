4478f0b3b4ca5aaf3552d40d71fa3cadf4d544b7
Marzycielx <marszalek_szymon@tlen.pl>
2024-05-21 06:40:48 +0200

    Update backup.sh
    translation of comments

--- backup.sh
+++ backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-limitsizelocal=85 # Limit miejsca na dysku ( w %), po przekroczeniu którego kopia nie utworzy się aby nie nadpisać plików 
-logFile='./logfile.txt' # Plik z Logami dotyczącymi Backupów
+limitsizelocal=85 # Disk space limit (in %), beyond which a copy will not be created to avoid overwriting the files
+logFile='./logfile.txt' # File with Backup Logs
 EXCLUDE_LIST="/boot"
-pathbackup="/backup" # Katalog docelowy gdzie mają być zapisywane kopie zapasowe
-pathmysql="/mysql" # Katalog docelowy gdzie mają być zapisywane kopie bazy danych
-savebackups="/etc /home /root /var /usr /bin" # Katalogi z których mają być robione kopie | oddzielone spacją 
-# Pamiętaj aby katalog gdzie utworzony będzie backup, nie był wpisany do katalogów z których ma być tworzony backup
+pathbackup="/backup" # Target directory where backups are to be saved
+pathmysql="/mysql" # Target directory where database copies are to be saved
+savebackups="/etc /home /root /var /usr /bin" # Directories from which copies should be made | separated by a space
+# Remember that the directory where the backup will be created should not be included in the directories from which the backup is to be created
 ###############################################
 
 
@@ -16,12 +16,12 @@ while read output;
 do
   usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
   if [ $usep -ge $limitsizelocal ] ; then
-		echo "[$DATA] Za mało miejsca na dysku aby utworzyć kopie zapasową. miejsce na dysku $usep%, limit $limitsizelocal%." >> "${logFile}"; 
-			exit 0
+		echo "[$DATA] There is not enough disk space to create a backup. disk space $usep%, limit $limitsizelocal%." >> "${logFile}"; 
+			exit 1
     else
 		tar --warning=no-file-changed --absolute-names -cpzf $pathbackup/$DATA.tar.gz $savebackups
 			mysqldump --defaults-file=/etc/mysql/debian.cnf --routines --flush-privileges --all-databases >$pathmysql/$DATA.sql
-				echo "[$DATA] Kopia została stworzona" >> "${logFile}"; 
+				echo "[$DATA] The copy has been created" >> "${logFile}"; 
 					exit 0
 	fi
 
@@ -29,7 +29,7 @@ done
 }
 
 Start_APK() {
-echo "[$DATA] Uruchomiono Skrypt" >> "${logFile}"; 
+echo "[$DATA] Script started" >> "${logFile}"; 
 find $pathbackup -mtime +7 -exec rm -R {} \; && find $pathmysql -mtime +7 -exec rm -R {} \;
 if [ "$EXCLUDE_LIST" != "" ] ; then
   df -hP |  grep -vE "^[^/]|tmpfs|cdrom|${EXCLUDE_LIST}" | awk '{print $5 " " $6}' | Start
@@ -38,10 +38,10 @@ fi
 
 case "$1" in
     start|Start|START) Start_APK ;;
-		* ) echo "Poprawne uzycie: $0 Start" >&2
+		* ) echo "Correct use: $0 Start" >&2
 	exit 1
     ;;
 esac
 
 # By Marzycielx - Marszałek Szymon 2020.
-# Pozdrawiam każdą osobę w każdych okolicznościach która to czyta :) 
+# Greetings to every person in all circumstances who reads this :)
